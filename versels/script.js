4f1cd736809a6d300ce45c03021af9c3c477e4f2
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-07-15 17:21:19 +0200

    Calculator second push

--- versels/script.js
+++ versels/script.js
@@ -1,5 +1,6 @@
 
 const answer = document.querySelector(".answer");
+const answero = document.querySelector(".answero");
 const buttons = document.querySelectorAll(".pursa-value");
 // console.log(buttons);
 const operations = document.querySelectorAll(".numbers button");
@@ -9,34 +10,76 @@ const equal = document.getElementById("equal");
 console.log(operators)
 let numberHolder = "";
 
+
+
 // numbers
 operations.forEach(a => a.addEventListener("click", function(e){
   numberHolder += e.target.innerText;
   console.log(numberHolder)
   updateDisplay();
 }));
- 
+
+
+
+
+let solution = ""; 
+let sol = 0;
 operators.forEach(a => a.addEventListener("click", function(e){
-    
-  numberHolder += e.target.innerText;
+  
+    const sign =e.target.innerText;
+    if(sign === "+"){
+    //   const toBeOpperated = numberHolder.split(/[/+/-/×/÷/=/]/).Number();
+     numberHolder += "+";
+    }else if(sign === '-'){
+     numberHolder += "-";
+    }else if(sign === '×'){
+     numberHolder += "*";
+    }else if(sign === '÷'){
+     numberHolder += "/";  
+    }else{
+        numberHolder += sign;
+    }
   console.log(numberHolder)
   updateDisplay();
 }));
+
+
+
 // OPPERATIONS
-equal.addEventListener("click", function(){
-    const toBeOpperated = numberHolder.split(/[/+/-/×/÷/=/]/);
+// equal.addEventListener("click", function(){
     
-    console.log(toBeOpperated);
+    
+//     console.log(toBeOpperated);
+// })
+
+
+
+buttons.forEach(a => a.addEventListener("click",function (e){
+
+   const text = e.target.innerText;
+   if(text === 'CE'){
+     answer.innerHTML = "";
+     numberHolder ="";
+    }else if(text === '%'){
+    numberHolder += "/100";
+    }else{
+        numberHolder += text;
+    }
+    console.log(text);
+}));
+
+
+equal.addEventListener("click", function(){
+    try{
+        const result = eval(numberHolder);
+        numberHolder = result.toString();
+        updateDisplay();
+
+    }catch(error){
+      numberHolder = "";
+      console.error(error);
+    }
 })
-
-
-
-// buttons.forEach(a => a.addEventListener("click",function (e){
-//    const text = e.target.innerText;
-//    answer.innerHTML = text;
-//     console.log(text);
-// }));
-
 function updateDisplay(){
     answer.textContent = numberHolder;
 }
